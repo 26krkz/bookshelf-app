c6fbd43f62a77f15454cdd7733813d0e5aff8253
26krkz <26krkz@gmail.com>
2025-02-12 23:04:54 +0900

    feat: app用Googleログインロジックを実装

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,28 @@
 import { StyleSheet } from "react-native";
 import { WebView } from "react-native-webview";
 import Constants from "expo-constants";
+import { useLoginOnGoogleAuth } from "@/hooks/useLoginOnGoogleAuth";
 
 export default function HomeScreen() {
-  return <WebView source={{ uri: "https://bookshelf-olive-mu.vercel.app/" }} />;
+  const { webViewRef, promptAsync } = useLoginOnGoogleAuth();
+  return (
+    <WebView
+      ref={webViewRef}
+      source={{ uri: "https://bookshelf-olive-mu.vercel.app/" }}
+      injectedJavaScript={`
+          window.requestGoogleLogin = function() {
+          window.ReactNativeWebView.postMessage("google-login");
+        };
+      `}
+      onMessage={(event) => {
+        const message = event.nativeEvent.data;
+        if (message === "google-login") {
+          promptAsync();
+        }
+      }}
+      userAgent="ExpoWebView"
+    />
+  );
 }
 
 const styles = StyleSheet.create({
